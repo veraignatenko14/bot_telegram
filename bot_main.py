d1b1358644685a290c5ba3533c6e7bf1829e1a68
petya <test@gmail.com>
2023-06-08 15:14:16 +0300

    Adding zodiac signs fetch

--- bot_main.py
+++ bot_main.py
@@ -1,5 +1,5 @@
 import telebot
-from jinja2 import Template
+import stuff
 
 
 bot = telebot.TeleBot('')
@@ -7,10 +7,39 @@ bot = telebot.TeleBot('')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    with open('templates/start.html', 'r', encoding='utf-8') as f:
-        text = f.read()
-    ans = Template(text)
-    bot.send_message(message.chat.id, ans.render(username=message.chat.username), parse_mode='html')
+    template = stuff.make_template('start')
+    username = message.chat.username
+    msg = template.render(username=username)
+    pin = bot.send_message(message.chat.id, ans.render(username=message.chat.username), parse_mode='html')
+    bot.pin_chat_message(message.chat.id, message_id=pin.id)
+
+
+@bot.message_handler(commands=['dog'])
+def send_dog(message):
+    img = stuff.get_dog_img()
+    bot.send_photo(message.chat.id, photo=img)
+
+
+@bot.message_handler(commands=['horo'])
+def get_horo(message):
+    signs = ['Aries ♈️', 'Taurus ♉️', 'Gemini ♊️', 'Cancer ♋️',
+             'Leo ♌️', 'Virgo ♍️', 'Libra ♎️', 'Scorpio ♏️',
+             'Sagittarius ♐️', 'Capricorn ♑️', 'Aquarius ♒️', 'Pisce ♓️']
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=3)
+    buttons = [telebot.types.KeyboardButton(sign) for sign in signs]
+    markup.add(*buttons)
+    bot.send_message(message.chat.id, 'Выбери свой знак зодиака: ', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def zodiac(message):
+    signs = ['Aries', 'Taurus', 'Gemini', 'Cancer',
+             'Leo', 'Virgo', 'Libra', 'Scorpio',
+             'Sagittarius', 'Capricorn', 'Aquarius', 'Pisce']
+    if message.text[:-3] in signs:
+        zodiac_sign = message.text[:-3]
+        res = stuff.parse_horo(zodiac_sign.lower())
+        bot.send_message(message.chat.id, text=res)
 
 
 if __name__ == '__main__':
--- stuff.py
+++ stuff.py
@@ -1,12 +1,28 @@
 import requests as r
+from jinja2 import Template
 
 
 def get_dog_img():
-  url = 'https://random.dog/woof.json'
-  resp = r.get(url).json()
-  return resp['url']
+ url = 'https://random.dog/woof.json'
+ resp = r.get(url).json()
+ return resp['url']
 
 
- def make_template(filename):
-  with open(f'')
+def make_template(filename):
+  with open(f'templates/{filename}.html', 'r', encoding='utf-8') as f:
+   text = f.read()
+  template = Template(text)
+  return template
 
+
+def parse_horo(sign):
+ url = f"https://horoscopes-ai.p.rapidapi.com/get_horoscope_en/{sign}/tomorrow/general"
+
+ headers = {
+  "X-RapidAPI-Key": "39b2fbbbfamshc9aeb391d0d535ap1e47c4jsn6ce62fa8951c",
+  "X-RapidAPI-Host": "horoscopes-ai.p.rapidapi.com"
+ }
+
+ response = r.get(url, headers=headers)
+ res = response.json()
+ return res['general'][0]
